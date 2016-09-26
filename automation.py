edd225ba4eb551549c5756c62579a7c508b6dd6a
sylvain-gdk <sylvain-gdk@users.noreply.github.com>
2016-09-26 09:58:05 -0400

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,3 +1,4 @@
+# Requires the initio.py library that can be found here: http://4tronix.co.uk/initio/
 
 import initio, time
 
@@ -11,29 +12,42 @@ pVal = 0 # 0 degrees is centre
 
 initio.init()
 
+# Moving tilt servo to simulate Yes (not used yet)
+# @pVal - passing the tilt value
 def doYes(tPan):
+    # @tilt - passing the pan pin
+    # @tVal - passing the pan value
     initio.setServo(pan, pVal)
     time.sleep(0.2)
 
+# Moving pan servo to simulate No  
+# @tVal - passing the tilt value
 def doNo(tVal):
+    # @tilt - passing the tilt pin
+    # @tVal - passing the tilt value
     initio.setServo(tilt, tVal)
     time.sleep(0.2)
 
 # main loop
 try:
+    # Sets values of infrared sensors
     lastIRleft = initio.irLeft()
     lastIRright = initio.irRight()
     lastIRall = initio.irAll()
     while True:
+        # Sets motors forward with speed value
         initio.forward(speed)
+        # Gets and prints distance from ultrasonic sensor
         distSonar = initio.getDistance()
         print "Sonar: ", int(distSonar)
         time.sleep(0.05)
         
+        # If ultrasonic sensor is triggered or both infrared sensors are triggered...
         if (distSonar < 30) or (lastIRall):
             newIRleft = initio.irLeft()
             newIRright = initio.irRight()
             
+            # If both infrared sensors are triggered, will backup creating an arc
             if (newIRleft != lastIRleft) and (newIRright != lastIRright):
                 initio.stop()
                 time.sleep(1.5)
@@ -48,7 +62,8 @@ try:
                 time.sleep(6)
                 initio.stop()
                 time.sleep(1.5)
-            
+                       
+            # If right sensor is triggered, will turn left
             elif (newIRleft == lastIRleft) and (newIRright != lastIRright):
                 initio.stop()
                 time.sleep(1.5)
@@ -64,6 +79,7 @@ try:
                 initio.stop()
                 time.sleep(1.5)
                 
+            # If left sensor is triggered, will turn right
             elif (newIRleft != lastIRleft) and (newIRright == lastIRright):
                 initio.stop()
                 doNo(-20)
